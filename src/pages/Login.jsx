2a47d592666d3d9b3bf0ff7bd57a373e7810c92d
Bruna <brunaalmeida075@gmail.com>
2022-11-03 17:22:33 -0300

    requisito 1 e 2

---
+++ src/pages/Login.jsx
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import { Redirect } from 'react-router-dom';
+import Carregando from './Carregando';
+
+class Login extends Component {
+  state = {
+    name: '',
+    isBtnDisabled: '',
+    carregando: false,
+    login: false,
+  };
+
+  ValidarBtn = ({ target }) => {
+    const { value } = target;
+    const numMin = 3;
+    this.setState({
+      name: value,
+      isBtnDisabled: value.length >= numMin,
+    });
+  };
+
+  SalvaLogin = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({
+      carregando: true,
+    });
+    await createUser({ name });
+    this.setState({
+      carregando: false,
+      login: true,
+    });
+  };
+
+  render() {
+    const { isBtnDisabled, carregando, login } = this.state;
+    return (
+      <div data-testid="page-login">
+        { carregando ? <Carregando />
+          : (
+            <form className="Login">
+              <input
+                className="nome"
+                type="text"
+                name="name"
+                data-testid="login-name-input"
+                placeholder="Digite seu nome"
+                onChange={ this.ValidarBtn }
+              />
+              <button
+                type="submit"
+                className="btnEntrar"
+                data-testid="login-submit-button"
+                onClick={ this.SalvaLogin }
+                disabled={ !isBtnDisabled }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+        {login && <Redirect to="/search" /> }
+      </div>
+    );
+  }
+}
+
+export default Login;
